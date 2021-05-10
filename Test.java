d75678a79c8a91aef26074632eddc58d78033b38
shishodia2008 <83969463+shishodia2008@users.noreply.github.com>
2021-05-10 21:28:45 +0530

    New Non-static Method Added

--- Test.java
+++ Test.java
@@ -1,7 +1,12 @@
 public class Test
 {
+  public void show()
+  {
+    System.out.println("Non-static Method");
+  }
   public static void main(String args[])
   {
     System.out.println("Welcome User");
+    new Test().show();
   }
 }
